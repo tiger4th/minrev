865fbeaa2d18e88892fa80c448187853f2c96508
tiger4th <tiger4th@gmail.com>
2015-03-15 18:44:31 +0900

    Amazonアソシエイト表記

--- index.php
+++ index.php
@@ -357,8 +357,8 @@ if ($id == 1) {
                         <hr class="comment-hr">
                         <div class="similar-search">
                             <span>似たもの検索: </span>
-                            <a href="http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3185576&pid=883213932&vc_url=<?php echo rawurlencode('http://search.shopping.yahoo.co.jp/search?ei=UTF-8&p='.$item["Target"]["Name"]); ?>" class="zocial yahoo small" target="_blank">Yahoo!で探す</a>
-                            <a href="http://www.amazon.co.jp/gp/search?ie=UTF8&camp=247&creative=1211&index=aps&keywords=<?php echo $item["Target"]["Name"]; ?>&linkCode=ur2&tag=tiger4th-22" class="zocial amazon small" target="_blank">Amazonで探す</a>
+                            <a href="http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3185576&pid=883213932&vc_url=<?php echo rawurlencode('http://search.shopping.yahoo.co.jp/search?ei=UTF-8&p='.$item["Target"]["Name"]); ?>" class="zocial yahoo" target="_blank">Yahoo!で探す</a>
+                            <a href="http://www.amazon.co.jp/gp/search?ie=UTF8&camp=247&creative=1211&index=aps&keywords=<?php echo $item["Target"]["Name"]; ?>&linkCode=ur2&tag=tiger4th-22" class="zocial amazon" target="_blank">Amazonで探す</a>
                         </div>
                     </div>
                     <div class="clear"></div>
@@ -400,6 +400,8 @@ if ($id == 1) {
     <a href="http://developer.yahoo.co.jp/about">
     <img src="http://i.yimg.jp/images/yjdn/yjdn_attbtn1_125_17.gif" title="Webサービス by Yahoo! JAPAN" alt="Web Services by Yahoo! JAPAN" width="125" height="17" border="0" style="margin:15px 15px 0px 15px"></a>
     <!-- End Yahoo! JAPAN Web Services Attribution Snippet -->
+    
+    <a href="http://www.amazon.co.jp/?tag=tiger4th-22" class="zocial amazon associate" target="_blank">Amazon.co.jpアソシエイト</a>
     
     <img Src="http://ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=3185576&pid=883213932" height="1" width="1" Border="0">
 
